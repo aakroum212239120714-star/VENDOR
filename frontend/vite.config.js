d84e87edb4b05ef0579f278356577796aee8988f
aakroum212239120714-star <a.akroum212239120714@cu-aflou.edu.dz>
2026-06-06 23:37:20 +0100

    fix: connect frontend to production backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,10 +10,7 @@ export default defineConfig({
         target: 'http://localhost:5000',
         changeOrigin: true,
       },
-      '/uploads': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
+      
     },
   },
 })
